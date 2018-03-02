544db69fb4ae383da3bc9b4668c46a839ef2bcf5
youssef nabil <youssefnabil48@outlook.com>
2018-03-03 00:33:19 +0200

    git ignore file added and file structure is set

--- api/controllers/calculationCTRL.js
+++
@@ -1,6 +0,0 @@
-module.exports.add = function(req, res) {
-    // body...
-    var result = parseInt(req.params.x) + parseInt(req.params.y);
-    //res.json({"result":result});
-    res.send(String(result));
-};
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 //importing controllers
-var calculator = require('../controllers/calculationCTRL');
+var test = require('../controllers/test.controller');
 
+//defining routes
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ title: 'Express app' });
 });
+router.get('/add/:x/:y',test.add);
 
-router.get('/add/:x/:y',calculator.add);
-
+//exporting router module
 module.exports = router;
