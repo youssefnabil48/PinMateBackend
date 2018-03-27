243fd673ebb27c22f9d96855d0cbdc44e8d7a7f6
sara <saraelsheemi93@yahoo.com>
2018-03-27 16:17:29 +0200

    models

--- api/models/trackers.model.js
+++ api/models/trackers.model.js
@@ -1,7 +1,18 @@
 var mongoose = require('mongoose');
 
 var TrackerSchema = new mongoose.Schema({
-
+    source : {
+      type : String,
+      required : true
+    },
+    destination : {
+      type : String,
+      required : true
+    },
+    created_at : {
+      type : Date,
+      default : Date.now()
+    }
 });
 
 mongoose.model('Tracker',TrackerSchema);
