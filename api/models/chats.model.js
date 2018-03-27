43e05957943d7fb23d6f36f8ad13300481720d3e
sara <saraelsheemi93@yahoo.com>
2018-03-27 15:09:55 +0200

    models

--- api/models/chats.model.js
+++ api/models/chats.model.js
@@ -1,7 +1,32 @@
 var mongoose = require('mongoose');
 
 var ChatSchema = new mongoose.Schema({
-
+  created_at : {
+    type : Date,
+   default :  Date.now()
+  },
+  content : {
+    type : String,
+    required: true
+  },
+  status : {
+    type : Boolean,
+    required : true
+  },
+  delivered : {
+    type : Boolean,
+    required : true
+  },
+  sender_id :  {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : User,
+    required : true
+  },
+  receiver_id : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : User,
+    required : true
+  }
 });
 
 mongoose.model('Chat',ChatSchema);
--- api/models/notifications.model.js
+++ api/models/notifications.model.js
@@ -1,6 +1,37 @@
 var mongoose = require('mongoose');
 
 var NotificationSchema = new mongoose.Schema({
+  receiver_id : {
+    type : mongoose.Schema.Type.ObjectId,
+    ref : User,
+    required : true;
+  },
+  created_at : {
+    type : Date,
+  default:  Date.now()
+  },
+
+  content : {
+    type : String,
+    required : true
+  },
+
+  status : {
+    type : Boolean,
+    required : true
+  },
+
+  Notification_Type : {
+    name : {
+      type : String,
+      required : true
+    },
+    
+    desription : {
+      type : String,
+      required : true
+    }
+  }
 
 });
 
