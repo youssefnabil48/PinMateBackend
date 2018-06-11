70da070fcfcc6cb63e8a0672be21e99b004798f1
sara <saraelsheemi93@yahoo.com>
2018-06-11 21:08:30 +0200

    notf

--- api/models/notifications.model.js
+++ api/models/notifications.model.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+var User = mongoose.model ('User');
+const PushNotifications = require('@pusher/push-notifications-server');
+
 
 var NotificationSchema = new mongoose.Schema({
   receiver_id : {
@@ -57,8 +60,32 @@ NotificationSchema.statics.getUserNotifications = function(){
     }
     Calling route:
 */
-NotificationSchema.statics.createNotification = function(){
+NotificationSchema.statics.sendNotification = async function(user,content){
+  try{
 
+    let pushNotifications = new PushNotifications({
+      instanceId: '27e97326-f21c-4a92-8713-1dda5cbc88e3',
+      secretKey: '669C8EA5410C253BB28A32C83579B86'
+    });
+    let publishResponse = await pushNotifications.publish(['hello'], {
+    apns: {
+      aps: {
+        alert: 'Hello!'
+      }
+    },
+    fcm: {
+      notification: {
+        title: 'Friend Request',
+        body: 'Ahmad sent you a friend request'
+      }
+    }
+  });
+    console.log('Just published:', publishResponse.publishId);
+  } catch (e)
+  {
+    console.log(e);
+    //throw e;
+  }
 }
 
 /*
