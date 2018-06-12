216247c42ab69788bbde3a7b8f97d5fdc6d6c214
HagarGadallah <hagargadallah@gmail.com>
2018-06-12 07:51:52 +0200

    hangout req and friend request ctrl

--- api/controllers/friendRequest.controller.js
+++ api/controllers/friendRequest.controller.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 var FriendRequest = mongoose.model('FriendRequest');
+const { validateAll } = require('indicative');
+
 
 
 /*
-    Description
+    Description : gets the requests of a specific receiver
     Takes:
     Returns: {
         error: "Error object if any",
@@ -11,7 +13,33 @@ var FriendRequest = mongoose.model('FriendRequest');
     }
     Calling route:
 */
-module.exports.getAll = function(req, res) {
+module.exports.getAll = async function(req, res) {
+    try {
+        var friendRequests = await FriendRequest.getUserRequests(req.params.rcvrId);
+        if(friendRequests.length <= 0){
+          res.status(200).json({
+            ok: true,
+            data: friendRequests,
+            message: 'No requests found',
+            error:null
+          });
+          return;
+        }
+        res.status(200).json({
+          ok: true,
+          data: friendRequests,
+          message: 'Requests loaded successfully',
+          error:null
+        });
+      } catch (e) {
+        console.log(e);
+        res.status(500).json({
+          ok: false,
+          data: null,
+          message: 'Internal server error',
+          error: e
+        });
+      }
 
 };
 
@@ -25,12 +53,54 @@ module.exports.getAll = function(req, res) {
     }
     Calling route:
 */
-module.exports.get = function(req, res) {
+module.exports.get = async function(req, res) {
+
+    try {
+        const rules = {
+          id: 'required'
+        };
+        await validateAll(req.params, rules);
+      } catch (e) {
+        console.log(e);
+        res.status(400).json({
+          ok: false,
+          data: null,
+          message: 'Validation error, bad request',
+          error: e
+        });
+        return;
+      }
+      try {
+        var friendReq = await FriendRequest.getRequestById(req.params.id);
+        if(!friendReq){
+          res.status(200).json({
+            ok: true,
+            data: friendReq,
+            message: 'Request not found',
+            error:null
+          });
+          return;
+        }
+        res.status(200).json({
+          ok: true,
+          data: friendReq,
+          message: 'Request loaded successfully',
+          error:null
+        });
+      } catch (e) {
+        console.log(e);
+        res.status(500).json({
+          ok: false,
+          data: null,
+          message: 'Internal server error',
+          error: e
+        });
+      }
 
 };
 
 /*
-    Description
+    Description : working but there is a small problem with validation, still under constuction
     Takes:
     Returns: {
         error: "Error object if any",
@@ -38,7 +108,58 @@ module.exports.get = function(req, res) {
     }
     Calling route:
 */
-module.exports.create = function(req, res) {
+module.exports.create = async function(req, res) {
+
+   /* try {
+        const rules = {
+          sender_id: 'required',
+          receiver_id : 'required'
+        };
+        await validateAll(req.params, rules);
+      } catch (e) {
+        console.log(e);
+        res.status(400).json({
+          ok: false,
+          data: null,
+          message: 'Validation error, bad request',
+          error: e
+        });
+        return;
+      }*/
+    try {
+        var friendRequest = new FriendRequest({
+           created_at : req.body.created_at,
+           sender_id : req.body.sender_id,
+           receiver_id : req.body.receiver_id,
+           status: req.body.status
+        });
+        // var check = module.exports.get(friendRequest);
+        // if(check != null){
+        //     res.status(200).json({
+        //         ok: false,
+        //         data: friendRequest,
+        //         message: 'Request already exits',
+        //         error: null
+        //     });
+        // }
+        // else {
+            let newFriendReq = await FriendRequest.create(friendRequest);
+            res.status(200).json({
+                ok: true,
+                data: newFriendReq,
+                message: 'Request sent',
+                error: null
+            });
+        }
+     catch (e) {
+        console.log(e);
+        res.status(500).json({
+            ok: false,
+            data: null,
+            message: 'Internal server error',
+            error: e
+        });
+    }
 
 };
 
@@ -57,7 +178,8 @@ module.exports.update = function(req, res) {
 };
 
 /*
-    Description
+    
+    Description : working but there is a small problem with validation, still under constuction
     Takes:
     Returns: {
         error: "Error object if any",
@@ -65,6 +187,33 @@ module.exports.update = function(req, res) {
     }
     Calling route:
 */
-module.exports.delete = function(req, res) {
+module.exports.delete = async function(req, res) {
+
+    try{
+        var friendRequest = await FriendRequest.deleteRequest(req.params.id);
+          if(!friendRequest){
+            res.status(200).json({
+              ok: true,
+              data: friendRequest,
+              message: 'Request was not deleted',
+              error:null
+            });
+            return;
+          }
+          res.status(200).json({
+            ok: true,
+            data: friendRequest,
+            message: 'Request deleted successfully',
+            error:null
+          });
+      }catch (e) {
+        console.log(e);
+        res.status(500).json({
+          ok: false,
+          data: null,
+          message: 'Internal server error',
+          error: e
+        });
+      }
 
 };
--- api/controllers/hangoutRequest.controller.js
+++ api/controllers/hangoutRequest.controller.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var HangoutRequest = mongoose.model('HangoutRequest');
+const { validateAll } = require('indicative');
 
 
 /*
@@ -11,53 +12,140 @@ var HangoutRequest = mongoose.model('HangoutRequest');
     }
     Calling route:
 */
-module.exports.getUserRequests = function(req, res) {
-  // var hangoutRequests = HangoutRequest.find(function(err, tests) {
-  //     if (err)
-  //         res.status(500).json({ error: err });
-  //     else {
-  //         if (!hangoutRequests) {
-  //             res.json({ msg: "no object found" });
-  //         } else {
-  //             res.json({
-  //                 msg: "success",
-  //                 hangoutRequests: hangoutRequests
-  //             });
-  //         }
-  //     }
-  // });
+module.exports.create = async function (req, res) {
+
+  try {
+      const rules = {
+          title: 'required|max:30',
+          created_by: 'required',
+          description: 'max:300'
+         // responded_by : 'required'
+        //  user_id: 'required', //responder id
+        //  status: 'required' // status
+      };
+      await validateAll(req.body, rules);
+  } catch (e) {
+      console.log(e);
+      res.status(400).json({
+          ok: false,
+          data: null,
+          message: 'Validation error, bad request',
+          error: e
+      });
+      return;
+  }
+
+  try {
+      var hangoutReq = new HangoutRequest({
+          title: req.body.title,
+          date: req.body.date,
+          description: req.body.description,
+          start_time: req.body.start_time,
+          created_by: req.body.created_by,
+          responded_by: req.body.responded_by
+      });
+      let newHangoutReq = await HangoutRequest.create(hangoutReq);
+      res.status(200).json({
+          ok: true,
+          data: newHangoutReq,
+          message: 'Hangout request created successfully',
+          error: null
+      });
+  } catch (e) {
+      console.log(e);
+      res.status(500).json({
+          ok: false,
+          data: null,
+          message: 'Internal server error',
+          error: e
+      });
+  }
 };
 
 
 /*
-    Description
-    Takes:
-    Returns: {
-        error: "Error object if any",
-        msg: "Success or failure message"
-    }
-    Calling route:
+  Description
+  Takes:
+  Returns: {
+      error: "Error object if any",
+      msg: "Success or failure message"
+  }
+  Calling route:
 */
-module.exports.get = function(req, res) {
-  var hangoutRequest = HangoutRequest({ _id: req.params.id });
-  var foundHangoutequest = HangoutRequest.findOne(hangoutRequest, function(err, foundHangoutequest) {
-      if (err)
-          res.status(500).json({ error: err });
-      else {
-          if (!foundHangoutequest) {
-              res.json({ msg: "no object found" });
-          } else {
-              res.json({
-                  msg: "success",
-                  hangoutRequests: foundHangoutequest
-              });
-          }
-      }
+module.exports.update = async function (req, res) {
+
+  try {
+      var hangoutReqId = req.params.id;
+      let newHangoutReq = await HangoutRequest.updateRequest(hangoutReqId,req.body);
+      if(!newHangoutReq){
+          res.status(200).json({
+            ok: true,
+            data: newHangoutReq,
+            message: 'Request was not updated',
+            error:null
+          });
+          return;
+        }
+      res.status(200).json({
+        ok: true,
+        data: newHangoutReq,
+        message: 'Request updated successfully',
+        error: null
     });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      ok: false,
+      data: null,
+      message: 'Internal server error',
+      error: e
+    });
+  }
+
 };
 
 /*
-    Description
+  Description
+  Takes:
+  Returns: {
+      error: "Error object if any",
+      msg: "Success or failure message"
+  }
+  Calling route:
+*/
+module.exports.delete = async function (req, res) {
+
+  try{
+      var hangoutRequests = await HangoutRequest.deleteRequest(req.params.id);
+        if(hangoutRequests == undefined){
+          res.status(200).json({
+            ok: true,
+            data: hangoutRequests,
+            message: 'Request was not deleted',
+            error:null
+          });
+          return;
+        }
+        res.status(200).json({
+          ok: true,
+          data: hangoutRequests,
+          message: 'Request deleted successfully',
+          error:null
+        });
+    }catch (e) {
+      console.log(e);
+      res.status(500).json({
+        ok: false,
+        data: null,
+        message: 'Internal server error',
+        error: e
+      });
+    }
+
+};
+
+/*
+    Description: Gets the requests of the creator
     Takes:
     Returns: {
         error: "Error object if any",
@@ -65,23 +153,33 @@ module.exports.get = function(req, res) {
     }
     Calling route:
 */
-module.exports.create = function(req, res) {
- console.log(req.body.title);
-  var h = new HangoutRequest({
-    title : req.body.title,
-//    date : req.body.date,
-    description : req.body.description,
-//    start_time : req.body.start_time,
-    created_by : req.body.created_by,
-    responded_by : req.body.responded_by
-  });
-  h.save(function(err) {
-      if (err) {
-          res.status(500).json({ error: err });
-      } else {
-          res.json({ msg: "success" });
+module.exports.getUserRequests = async function (req, res) {
+    try {
+        var hangoutRequests = await HangoutRequest.getUserRequests(req.params.userId);
+        if(hangoutRequests.length <= 0){
+          res.status(200).json({
+            ok: true,
+            data: hangoutRequests,
+            message: 'No requests found',
+            error:null
+          });
+          return;
+        }
+        res.status(200).json({
+          ok: true,
+          data: hangoutRequests,
+          message: 'Requests loaded successfully',
+          error:null
+        });
+      } catch (e) {
+        console.log(e);
+        res.status(500).json({
+          ok: false,
+          data: null,
+          message: 'Internal server error',
+          error: e
+        });
       }
-  });
 };
 
 
@@ -94,8 +192,48 @@ module.exports.create = function(req, res) {
     }
     Calling route:
 */
-module.exports.update = function(req, res) {
-
+module.exports.get = async function (req, res) {
+    try {
+        const rules = {
+          id: 'required'
+        };
+        await validateAll(req.params, rules);
+      } catch (e) {
+        console.log(e);
+        res.status(400).json({
+          ok: false,
+          data: null,
+          message: 'Validation error, bad request',
+          error: e
+        });
+        return;
+      }
+      try {
+        var hangoutReq = await HangoutRequest.getRequestById(req.params.id);
+        if(!hangoutReq){
+          res.status(200).json({
+            ok: true,
+            data: hangoutReq,
+            message: 'Request not found',
+            error:null
+          });
+          return;
+        }
+        res.status(200).json({
+          ok: true,
+          data: hangoutReq,
+          message: 'Request loaded successfully',
+          error:null
+        });
+      } catch (e) {
+        console.log(e);
+        res.status(500).json({
+          ok: false,
+          data: null,
+          message: 'Internal server error',
+          error: e
+        });
+      }
 };
 
 /*
@@ -107,6 +245,40 @@ module.exports.update = function(req, res) {
     }
     Calling route:
 */
-module.exports.delete = function(req, res) {
+module.exports.respond = async function(req,res){
 
-};
+  //Under construction still, not final yet
+  try {
+    var hangoutReqStatus = req.params.status;
+    var hangoutReq = await HangoutRequest.getUserRequests(req.params.userId);
+      if(hangoutReqStatus == true){
+        res.status(200).json({
+          ok: true,
+          data: hangoutReq,
+          message: 'Request accepted',
+          error:null
+        });
+      }
+
+      else if(hangoutReqStatus == false){
+        res.status(200).json({
+          ok: true,
+          data: hangoutReq,
+          message: 'Request rejected',
+          error:null
+        });
+        
+      }
+          
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      ok: false,
+      data: null,
+      message: 'Internal server error',
+      error: e
+    });
+}
+}
+
+
