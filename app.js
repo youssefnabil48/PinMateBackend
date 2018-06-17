3063da6cea4aee230e3eb6b3d21b6cbdc230edde
HagarGadallah <hagargadallah@gmail.com>
2018-06-17 02:15:32 +0200

    logs + hangout rqst ctrl modifications

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
+var fs = require('fs');
+
 
 var routes = require('./api/routes');
 var app = express();
+
+//For logs
+var accessLogStream = fs.createWriteStream(path.join( './logs/', 'access.log'), {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}))
+app.use(morgan('dev'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +22,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
