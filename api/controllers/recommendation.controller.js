a858acdff6f219f4f5db6c32e7288625433ee4e5
youssefnabil48 <youssefnabil48@outlook.com>
2018-07-06 05:33:36 +0200

    recommendation based on friends favorites

--- api/controllers/recommendation.controller.js
+++ api/controllers/recommendation.controller.js
@@ -18,10 +18,24 @@ module.exports.recommendPlace = async function(req,res){
                 }
             }
         }
-        
+        var sortedPlacesIds = sortProperties(rankedPlacesIds);
+        var places = await Place.find({
+            $or:[
+                {_id: mongoose.Types.ObjectId(sortedPlacesIds[0])},
+                {_id: mongoose.Types.ObjectId(sortedPlacesIds[1])},
+                {_id: mongoose.Types.ObjectId(sortedPlacesIds[2])}
+              ]
+        });
+        var result;
+        for(let i = 0; i<3; i++){
+            result = places.filter(function( obj ) {
+                return obj._id == sortedPlacesIds[i];
+            });
+            rankedPlaces.push(result[0]);
+        }
         res.status(200).json({
             ok: true,
-            data: rankedPlacesIds,
+            data: rankedPlaces,
             message: 'Recommended places',
             error: null
           });
@@ -31,8 +45,27 @@ module.exports.recommendPlace = async function(req,res){
             ok: false,
             data: null,
             message: 'Internal server error',
-            error: e
+            error: error
           });
     }
     
+}
+
+//this should sort keys but return the objects 
+//after modifications it return the keys only sorted according to the values
+function sortProperties(obj)
+{
+  // convert object into array
+	var sortable=[];
+	for(var key in obj)
+        if(obj.hasOwnProperty(key))
+            //sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+			sortable.push(key); //modified line
+	
+	// sort items by value
+	sortable.sort(function(a, b)
+	{
+	  return b[1]-a[1]; // compare numbers
+	});
+	return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
 }
